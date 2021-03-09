34fb90c30247cc5bfe31457e7c0fcdb3477b211b
Spencer Clemens <Soclemens@gmail.com>
2021-03-08 17:49:14 -0700

    final fix for parking lot app. From here should be easy to add template

--- src/BestPlace/urls.py
+++ src/BestPlace/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
-
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include("parkingGenie.urls")),
 ]
 
