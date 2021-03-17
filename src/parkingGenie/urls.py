8f5b1677300f2947940b7b7bf26673a0056b732a
Spencer Kasteler <sdkasteler@gmail.com>
2021-03-17 11:15:43 -0600

    commit for lot search and event search pages

--- src/parkingGenie/urls.py
+++ src/parkingGenie/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('addEvent', views.addEvent, name='addEvent'),
     path('addLot', views.addLot, name='addLot'),
+    path('events', views.searchEvents, name='searchEvents'),
+    path('event/<int:event_id>', views.lotSearch, name='lotSearch'),
+
 ]
