b35c50e976445461eb89f62cabdd20cd2de05b51
Icemanjr <holst.nathan@gmail.com>
2021-03-18 10:58:12 -0600

    modified the manageAccount.html file and made changes for Account database

--- src/parkingGenie/models.py
+++ src/parkingGenie/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    password = models.IntegerField()
+    acntInfo = JSONField()
