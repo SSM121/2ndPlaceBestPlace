4b3d5bd49a8665753482feb533cfcf2cfbce7670
Icemanjr <holst.nathan@gmail.com>
2021-03-18 11:11:21 -0600

    Realized I forgot the account type field in the Account Database

--- src/parkingGenie/models.py
+++ src/parkingGenie/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
+    accntType = models.CharField)max_length=10)
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=254)
     password = models.IntegerField()
-    acntInfo = JSONField()
+    accntInfo = JSONField()
