8a7e14c53a6681c94b749d7b9795a39fd2e787c4
Icemanjr <holst.nathan@gmail.com>
2021-03-23 21:24:59 -0600

    Created Parking Lot Database, has 'Event' attribute commented out until implementation of Event Database

--- src/parkingGenie/models.py
+++ src/parkingGenie/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse  # used to generate URLs by reversing the URL patterns
 import uuid  # required for Accounts to have a unique instance
 
+
 class Account(models.Model):
     # Fields
     accntType = models.CharField(max_length=10, help_text='The type of Account: User, Owner, or Manager')
@@ -16,9 +17,32 @@ class Account(models.Model):
 
     def __str__(self):  # The __str__ method is useful  so if we want to print out an account we can.
         return self.name.__str__() + " owns an account type of: " + self.accntType.__str__() \
-               + ". The email attatched to the account is " + self.email.__str__()
+               + ". The email attached to the account is " + self.email.__str__()
 
     def get_absolute_url(self):  # Not currently implemented Spencer Clemens working through a tutorial
-        """ Returns the url to access a detailed recored of an account"""
+        """ Returns the url to access a detailed record of an account"""
         return reverse('account-detail', args=[str(self.email)])
 
+
+class ParkingLot(models.Model):
+    # Fields
+    name = models.CharField(max_length=20, help_text="The name of the parking lot")
+    address = models.CharField(max_length=100, help_text="Address of the parking lot")
+    parking = models.IntegerField(help_text="Number of available normal parking spaces")
+    tailgate = models.IntegerField(help_text="Number of available tailgate parking spaces")
+    owner = models.ForeignKey(Account, on_delete=models.CASCADE, help_text="Owner of the parking lot")
+    # event = models.ManyToManyField(Event, help_text="Event(s) that will use this parking lot")    # Needs Event DB
+    date = models.DateField(help_text="Date of the Event")
+
+    def __str__(self):  # Useful for printing out Name and Address of the parking lot
+        return "%s \n %s" % (self.name, self.address)
+        # return [self.name, self.address]  # If we find this works better for our purposes.
+
+    def availSpots(self):   # Get the number of available normal parking spots
+        return parking
+
+    def availTailgate(self):    # Get the number of available tailgate parking spots
+        return tailgate
+
+    def isFull(self):   # True if parking lot has no more available parking spots
+        return parking == 0 and tailgate == 0
