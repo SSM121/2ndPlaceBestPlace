9b332c1d2e8cd7b989f130a31a55178b6d6441c0
Icemanjr <holst.nathan@gmail.com>
2021-04-01 11:07:30 -0600

    Modifications for Account and creating a superuser

--- src/parkingGenie/views.py
+++ src/parkingGenie/views.py
@@ -38,7 +38,7 @@ def register(request):
         terms = request.POST.get("terms")
         deals = request.POST.get("deals")
         if password1 == password2 and terms == "checked":
-            user = User.objects.create_user(userName, userEmail, userType, password1)
+            user = Account.objects.create_user(userName, userEmail, userType, password1)
             user.username = userName
             user.last_name = userLast
             user.userType = userType  # Commented out because user doesnt have the needed attribute
